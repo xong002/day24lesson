63798165b8f854c9562842c33429b5370d8e4340
Xin Zhi <115723006+xong002@users.noreply.github.com>
2023-06-26 11:42:06 +0800

    add createAccount to BankAccountRepository

--- src/main/java/sg/edu/nus/iss/day24lesson/repository/BankAccountRepository.java
+++ src/main/java/sg/edu/nus/iss/day24lesson/repository/BankAccountRepository.java
@@ -1,8 +1,15 @@
 package sg.edu.nus.iss.day24lesson.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.iss.day24lesson.model.BankAccount;
@@ -30,5 +37,28 @@ public class BankAccountRepository {
     public Boolean deposit(int id, Float depositAmount){
         return jdbcTemplate.update(DEPOSIT_SQL, depositAmount, id) > 0 ? true : false;
     }
+
+    public Integer createAccount(BankAccount bankAccount){
+        KeyHolder generatedKey = new GeneratedKeyHolder();
+        PreparedStatementCreator psc = new PreparedStatementCreator() {
+
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement ps = con.prepareStatement(CREATE_ACCOUNT_SQL, new String[] {"id"});
+                ps.setString(1, bankAccount.getFullName());
+                ps.setBoolean(2, bankAccount.getIsBlocked());
+                ps.setBoolean(3,bankAccount.getIsActive());
+                ps.setString(4, bankAccount.getAccountType());
+                ps.setFloat(5, bankAccount.getBalance());
+                return ps;
+            }
+            
+        };
+        jdbcTemplate.update(psc, generatedKey);
+        return generatedKey.getKey().intValue();
+    }
+
+    //alternatively, if no need to return id:
+    // return jdbcTemplate.update(CREATE_ACCOUNT_SQL, bankAccount.getFullName(), bankAccount.getIsBlocked(), bankAccount.getIsActive(), bankAccount.getAccountType(), bankAccount.getBalance()) > 0 ? true : false;
 
 }
